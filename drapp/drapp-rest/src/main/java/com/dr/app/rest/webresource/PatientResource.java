d0931f4695ac64356a95b1df7f8f14baa2b19da1
thesyedr <rahmath.syed@outlook.com>
2015-08-15 20:32:34 +0530

    api to get patient by id and and all patient records implemented

--- drapp/drapp-rest/src/main/java/com/dr/app/rest/webresource/PatientResource.java
+++ drapp/drapp-rest/src/main/java/com/dr/app/rest/webresource/PatientResource.java
@@ -2,8 +2,10 @@ package com.dr.app.rest.webresource;
 
 import javax.jws.WebService;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -20,4 +22,13 @@ public interface PatientResource {
 	@Path("/enroll")
 	public Response patientEnrollement(PatientEnrollementRequest patientEnrollementRequest);
 
+	@GET
+	@Produces({MediaType.APPLICATION_JSON})
+	@Path("/patient/{patientId}")
+	public Response getPatientRecordById(@PathParam("patientId") String patientId);
+	
+	@GET
+	@Produces({MediaType.APPLICATION_JSON})
+	@Path("/patients")
+	public Response getAllPatients();
 }
--- drapp/drapp-rest/src/main/java/com/dr/app/rest/webresourceImpl/PatientEnrollementResourceImpl.java
+++ drapp/drapp-rest/src/main/java/com/dr/app/rest/webresourceImpl/PatientEnrollementResourceImpl.java
@@ -1,5 +1,9 @@
 package com.dr.app.rest.webresourceImpl;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 import javax.ws.rs.core.Response;
 
 import org.apache.log4j.Logger;
@@ -11,6 +15,8 @@ import com.dr.app.repo.entity.Patient;
 import com.dr.app.rest.converters.PatientRequestConverters;
 import com.dr.app.rest.requests.PatientEnrollementRequest;
 import com.dr.app.rest.responses.DrResponse;
+import com.dr.app.rest.responses.PatientResponse;
+import com.dr.app.rest.responses.PatientsReponse;
 import com.dr.app.rest.webresource.PatientResource;
 import com.dr.app.service.PatientService;
 
@@ -21,11 +27,10 @@ public class PatientEnrollementResourceImpl implements PatientResource{
 	
 	@Autowired
 	private PatientRequestConverters patientRequestConverters;
-	
 	private static Logger logger = Logger.getLogger(PatientEnrollementResourceImpl.class);
+	
 	@Override
 	public Response patientEnrollement(PatientEnrollementRequest patientEnrollementRequest) {
-		
 		logger.info("Start of PatientEnrollement service");
 		Patient patient = patientRequestConverters.getPatientFromPatientWebRequest(patientEnrollementRequest);
 		Contacts contacts = patientRequestConverters.getPatientContacts(patientEnrollementRequest);
@@ -42,4 +47,66 @@ public class PatientEnrollementResourceImpl implements PatientResource{
 		return Response.ok(response).build();
 	}
 
+	@Override
+	public Response getPatientRecordById(String patientId) {
+		
+		Patient patientRecord = null;
+		PatientResponse patientResponse = new PatientResponse();
+		logger.info("Request processing start for patientId : "+patientId);
+		Integer patientID = Integer.parseInt(patientId);
+		if(patientID != null){
+			patientRecord = patientService.getPatientRecordById(patientID);
+			if(patientRecord != null ){
+				logger.info("recieved record for patient id : "+patientID);
+				patientResponse.setStatus(200);
+				patientResponse.setMessage("Patient Record fetched successfully");
+				patientResponse.setPatientId(patientRecord.getPatientId());
+				patientResponse.setPatientName(patientRecord.getPatientName());
+				patientResponse.setPatientContact(patientRecord.getContacts());
+				patientRecord.getContacts().setPatient(null);
+				patientResponse.setPatientInsurance(patientRecord.getInsurance());
+				patientRecord.getInsurance().setPatient(null);
+			
+			}else{
+				logger.info("record not available for patient:"+patientID);
+				patientResponse.setStatus(401);
+				patientResponse.setMessage("Patient Record Not found");
+			}
+		}else{
+			patientResponse.setStatus(400);
+			patientResponse.setMessage("Patient Record fetched failed");
+		}
+		logger.info("Sending Response for patient id : "+patientId);
+		return Response.ok(patientResponse).build();
+	}
+
+	@Override
+	public Response getAllPatients() {
+		logger.info("Request processing started for getting all the available patients");
+		List<Patient> patientsList = patientService.getAllAvailablePatients();
+		List<PatientResponse> patientResposneList = new ArrayList<PatientResponse>();
+		PatientsReponse patientsReponse = new PatientsReponse();
+		for(Patient patient : patientsList){
+			PatientResponse patientResponse = new PatientResponse();
+			patientResponse.setPatientId(patient.getPatientId());
+			patientResponse.setPatientName(patient.getPatientName());
+			patientResponse.setPatientContact(patient.getContacts());
+			patient.getContacts().setPatient(null);
+			patientResponse.setPatientInsurance(patient.getInsurance());
+			patient.getInsurance().setPatient(null);
+			patientResposneList.add(patientResponse);
+		}
+		if(patientResposneList.size() > 0){
+			logger.info("One or more Patients Records Recieved");
+			patientsReponse.setStatus(200);
+			patientsReponse.setMessage("Patients Record Recieved");
+			patientsReponse.setPatients(patientResposneList);
+		}else{
+			logger.info("Patients records are not abailable");
+			patientsReponse.setStatus(401);
+			patientsReponse.setMessage("Patients records are not abailable");
+		}
+		logger.info("Sending the Response for available patients");
+		return Response.ok(patientsReponse).build();
+	}
 }
